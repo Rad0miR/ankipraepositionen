739a7fd0025e815a13558c37819abc9155c5e66e
Radomyr Pershyn <pershin.radomir@gmail.com>
2024-05-21 16:46:02 +0300

    Preparations before first migration.

---
+++ PraepositionenVerbformenWPF/Data/NotesContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestForEF.Models;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace PraepositionenVerbformenWPF.Data
+{
+    internal class NotesContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["DBforPrepositions"].ConnectionString);
+        }
+    }
+}
---
+++ PraepositionenVerbformenWPF/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForEF.Models
+{
+    internal class Note
+    {
+        public int Id { get; set; }
+
+        public string GermanShort { get; set; } = null!;
+
+        public string GermanFull { get; set; } = null!;
+
+        public string Definition { get; set; } = null!;
+
+        public string? Example { get; set; }
+    }
+}
